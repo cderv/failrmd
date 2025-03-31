01336b868f031e548f349cdc6262ca1e9032ec22
Christophe Dervieux <christophe.dervieux@gmail.com>
2025-03-31 17:27:27 +0200

    test dev knitr

--- R/f.R
+++ R/f.R
@@ -3,3 +3,10 @@
 f <- function() g()
 g <- function() h()
 h <- function() rlang::abort("This is an error!")
+
+#' This is a useful function
+#' @export
+f2 <- function() g2()
+g2 <- function() h2()
+h2 <- function() stop("This is an error!")
+
